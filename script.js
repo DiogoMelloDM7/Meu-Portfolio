4fe2212b54c165a5ee94d3db1064482c4c4f53f1
Diogo Mello <diogomellodm7@gmail.com>
2025-02-16 11:35:36 -0300

    Alterando curriculo

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", function() {
         css3: "Cascading Style Sheets (CSS) é uma linguagem de estilo usada para descrever a apresentação de um documento escrito em HTML.",
         git: "Git é um sistema de controle de versão distribuído usado para rastrear mudanças em arquivos de código-fonte durante o desenvolvimento de software.",
         html: "HTML é uma linguagem de marcação utilizada na construção de páginas na Web.",
-        java: "Java é uma linguagem de programação orientada a objetos desenvolvida na década de 90 por uma equipe de programadores chefiada por James Gosling.",
+        fluig: "O Fluig é uma plataforma da TOTVS para gestão de processos, documentos e colaboração, permitindo automação via JavaScript e integração com outros sistemas.",
         javascript: "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.",
-        postgres: "PostgreSQL é um sistema gerenciador de banco de dados objeto relacional, desenvolvido como projeto de código aberto.",
+        postgres: "O SQL Server é um sistema de gerenciamento de banco de dados relacional (SGBD) desenvolvido pela Microsoft, usado para armazenar e manipular dados de forma segura e escalável.",
         python:"Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte. Foi lançada por Guido van Rossum em 1991.",
         django: "Django é um framework para desenvolvimento rápido para web, escrito em Python, que utiliza o padrão model-template-view."
         
